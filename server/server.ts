5de8904e9febbc651579af84f9d320731fc6781d
Ishant Bhurani <67356291+ishantbhurani@users.noreply.github.com>
2023-09-17 21:16:17 +0530

    add basic middlewares

--- server/server.ts
+++ server/server.ts
@@ -3,9 +3,16 @@ import { config } from 'dotenv'
 config()
 
 const PORT = process.env.PORT || 5000
-
 const app = express()
 
-app.get('/', (req, res) => res.send('Server is ready!'))
+// middlewares
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+// basic logger middleware
+app.get('/', (req, res, next) => {
+  console.log(req.method, req.originalUrl)
+  next()
+})
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
