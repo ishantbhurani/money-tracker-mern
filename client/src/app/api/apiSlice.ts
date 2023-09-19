42e7f5978d3f4fb5d292c050a6340eb9c3ab03ad
Ishant Bhurani <67356291+ishantbhurani@users.noreply.github.com>
2023-09-19 21:53:37 +0530

    update base url to deployed server

--- client/src/app/api/apiSlice.ts
+++ client/src/app/api/apiSlice.ts
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/dist/query/react'
 const apiSlice = createApi({
   reducerPath: 'api',
   baseQuery: fetchBaseQuery({
-    baseUrl: 'http://localhost:5000',
+    baseUrl: 'https://money-tracker-api-jkxz.onrender.com',
     credentials: 'include',
   }),
   tagTypes: ['Transaction'],
