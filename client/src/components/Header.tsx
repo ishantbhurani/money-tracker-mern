d7fccd6517e599da234f777189928ce51104f64e
Ishant Bhurani <67356291+ishantbhurani@users.noreply.github.com>
2023-09-19 10:14:41 +0530

    add nav to header

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,6 +1,24 @@
-import { Link } from 'react-router-dom'
+import { Link, NavLink, useNavigate } from 'react-router-dom'
+import { useAppSelector } from '../app/hooks'
+import { useLogoutMutation } from '../features/auth/authApiSlice'
+import { cn } from '../utils'
 
 export default function Header() {
+  const { userInfo } = useAppSelector(state => state.auth)
+
+  const navigate = useNavigate()
+
+  const [logoutApi, { isLoading }] = useLogoutMutation()
+
+  const handleClick = async () => {
+    try {
+      await logoutApi().unwrap()
+      navigate('/login')
+    } catch (err) {
+      console.error(err)
+    }
+  }
+
   return (
     <header className='container mx-auto flex items-center justify-between gap-4 border-b p-4 text-gray-900 sm:text-lg'>
       <div>
@@ -8,6 +26,51 @@ export default function Header() {
           Expenses
         </Link>
       </div>
+      <nav>
+        <ul className='flex items-center justify-center gap-4'>
+          {userInfo ? (
+            <>
+              <li>{userInfo.name}</li>
+              <li>
+                <button
+                  onClick={handleClick}
+                  disabled={isLoading}
+                  className='rounded border border-indigo-600 px-2 py-1.5 text-indigo-600 outline-none hover:bg-indigo-600 hover:text-white focus-visible:bg-indigo-600 focus-visible:text-white sm:p-2'
+                >
+                  Logout
+                </button>
+              </li>
+            </>
+          ) : (
+            <>
+              <li>
+                <NavLink
+                  to='login'
+                  className={({ isActive }) =>
+                    cn('hover:text-indigo-500', {
+                      'text-indigo-600': isActive,
+                    })
+                  }
+                >
+                  Login
+                </NavLink>
+              </li>
+              <li>
+                <NavLink
+                  to='register'
+                  className={({ isActive }) =>
+                    cn('text-lg hover:text-indigo-500', {
+                      'text-indigo-600': isActive,
+                    })
+                  }
+                >
+                  Register
+                </NavLink>
+              </li>
+            </>
+          )}
+        </ul>
+      </nav>
     </header>
   )
 }
