0d1905443ace8f97defba634763b3a5dd5ebb1b7
Ishant Bhurani <67356291+ishantbhurani@users.noreply.github.com>
2023-09-19 19:18:51 +0530

    add floating action button

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,11 @@
+import FAB from './components/FAB'
 import TransactionsList from './features/transactions/TransactionsList'
 
 export default function App() {
   return (
     <div className='container mx-auto flex-1'>
       <TransactionsList />
+      <FAB />
     </div>
   )
 }
