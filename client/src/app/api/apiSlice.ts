606c7ded9df2295f15ee8598da3993553b122f70
Ishant Bhurani <67356291+ishantbhurani@users.noreply.github.com>
2023-09-19 22:24:26 +0530

    update base url back to Render

--- client/src/app/api/apiSlice.ts
+++ client/src/app/api/apiSlice.ts
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/dist/query/react'
 const apiSlice = createApi({
   reducerPath: 'api',
   baseQuery: fetchBaseQuery({
-    baseUrl: 'https://money-tracker-mern.vercel.app/',
+    baseUrl: 'https://money-tracker-api-jkxz.onrender.com',
     credentials: 'include',
   }),
   tagTypes: ['Transaction'],
